77813b3928fed7df323d3c420f0ea26345b1ff9e
JosipBuselic <josip.busilica@gmail.com>
2025-03-24 15:40:09 +0100

    cart numbers going up

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+let countForCart = 0;
 document.addEventListener("DOMContentLoaded", () =>{
     
     window.addEventListener("scroll", () => {
@@ -13,4 +13,17 @@ document.addEventListener("DOMContentLoaded", () =>{
     document.getElementById("top_button").addEventListener("click", () => {
         window.scrollTo({top: 0, behavior: "smooth"});
     });
+
+    
+    const sizeElements = document.getElementsByClassName("size");
+    for (let i = 0; i < sizeElements.length; i++) {
+        sizeElements[i].addEventListener("click", () => {
+            const circle = document.getElementById("circle");
+            circle.innerHTML = ++countForCart;
+            if (countForCart !== 0) {
+                circle.style.display = "flex";
+                circle.style.textAlign = "center";
+            }
+        });
+    }
 });
