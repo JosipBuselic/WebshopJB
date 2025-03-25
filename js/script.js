dd8d5b0cf52204cc77f6edcd3775efb516cf2878
JosipBuselic <josip.busilica@gmail.com>
2025-03-25 19:08:38 +0100

    second body

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     
     const sizeElements = document.getElementsByClassName("size");
     let countForCart = 0;
-    
+
     for (let i = 0; i < sizeElements.length; i++) {
         sizeElements[i].addEventListener("click", () => {
             const circle = document.getElementById("circle");
@@ -67,6 +67,7 @@ document.addEventListener("DOMContentLoaded", () =>{
         const kategorije_links = document.getElementById("kategorije_links");
 
         kategorije_section.style.width = "20%";
+        kategorije_links.style.display = "flex";
         setTimeout(() => {
             kategorije_links.style.opacity = "1";
         }, 300);
@@ -78,7 +79,69 @@ document.addEventListener("DOMContentLoaded", () =>{
 
         kategorije_section.style.width = "0";
         kategorije_links.style.opacity = "0";
+        setTimeout(() => {
+            kategorije_links.style.display = "none";
+        },300);
     });
+
     
+    // Image slider
+    const first = document.getElementById("first_circle");
+    const second = document.getElementById("second_circle");
+    const third = document.getElementById("third_circle");
+
+    const row1 = document.getElementById("row1");
+    const row2 = document.getElementById("row2");
+    const row3 = document.getElementById("row3");
+    first.addEventListener("click", () => {
+        first.style.backgroundColor = "var(--tamno_plava)";
+        second.style.backgroundColor = "var(--tamno_plava_lower)";
+        third.style.backgroundColor = "var(--tamno_plava_lower)";
+
+        row1.style.display = "flex";
+        row2.style.display = "none";
+        row3.style.display = "none";
+    
+        setTimeout(() => {
+            row1.style.opacity = "1";
+            row2.style.opacity = "0";
+            row3.style.opacity = "0";
+        }, 50);
+
+   });
+    
+   second.addEventListener("click", () => {
+    first.style.backgroundColor = "var(--tamno_plava_lower)";
+    second.style.backgroundColor = "var(--tamno_plava)";
+    third.style.backgroundColor = "var(--tamno_plava_lower)";
+
+    row1.style.display = "none";
+    row2.style.display = "flex";
+    row3.style.display = "none";
+
+    setTimeout(() => {
+        row1.style.opacity = "0";
+        row2.style.opacity = "1";
+        row3.style.opacity = "0";
+    }, 50);
+
+   });
+
+   third.addEventListener("click", () => {
+    first.style.backgroundColor = "var(--tamno_plava_lower)";
+    second.style.backgroundColor = "var(--tamno_plava_lower)";
+    third.style.backgroundColor = "var(--tamno_plava)";
+
+    row1.style.display = "none";
+    row2.style.display = "none";
+    row3.style.display = "flex";
+
+    setTimeout(() => {
+        row1.style.opacity = "0";
+        row2.style.opacity = "0";
+        row3.style.opacity = "1";
+    }, 50);
+
+   });
 
 });
