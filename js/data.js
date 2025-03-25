db99de3a1b0d0177daf5ed68fd08e481e2fa3bc5
JosipBuselic <josip.busilica@gmail.com>
2025-03-25 13:59:35 +0100

    slike i data set

---
+++ js/data.js
@@ -0,0 +1,115 @@
+const data = {
+    "website": "odjeca",
+    "categories":[{
+
+        "name":"majice",
+        "products":[{
+            "name": "crna majica",
+            "image":"../images/products/majice/crna_majica",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "bijela majica",
+            "image":"../images/products/majice/bijela_majica",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "zelena majica",
+            "image":"../images/products/majice/bijela_majica",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "siva majica",
+            "image":"../images/products/majice/bijela_majica",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "siva majica",
+            "image":"../images/products/majice/roza_majica",
+            "size":["XS","S","M","L","XL","XXL"]
+        }]
+    },
+    {
+
+        "name":"hlace",
+        "products":[{
+            "name": "bijele hlace",
+            "image":"../images/products/hlace/bijele_hlace",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "smede hlace",
+            "image":"../images/products/hlace/smede_hlace",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "crne hlace",
+            "image":"../images/products/hlace/crne_hlace",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "plave hlace",
+            "image":"../images/products/hlace/plave_hlace",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "sive hlace",
+            "image":"../images/products/hlace/sive_hlace",
+            "size":["XS","S","M","L","XL","XXL"]
+        }]
+    },
+    {
+
+        "name":"kape",
+        "products":[{
+
+            "name": "USA kapa",
+            "image":"../images/products/kape/Usa_kapa",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "plava kapa",
+            "image":"../images/products/kape/plava_kapa",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "zuta kapa",
+            "image":"../images/products/kape/zuta_kapa",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "crna kapa",
+            "image":"../images/products/kape/crna_kapa",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "zelena kapa",
+            "image":"../images/products/kape/zelena_kapa",
+            "size":["XS","S","M","L","XL","XXL"]
+        }]
+    },
+    {
+        "name":"Trenike",
+        "products":[{
+
+            "name": "crna trenirka",
+            "image":"../images/products/trenirka/crna_trenirka",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "smeda trenirka",
+            "image":"../images/products/trenirka/smeda_trenirka",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "plava trenirka",
+            "image":"../images/products/trenirka/plava_trenirka",
+            "size":["XS","S","M","L","XL","XXL"]
+        },
+        {
+            "name": "siva trenirka",
+            "image":"../images/products/trenirka/siva_trenirka",
+            "size":["XS","S","M","L","XL","XXL"]
+        }]
+    }]
+}
