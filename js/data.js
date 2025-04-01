28bd67ed7ca38729feb45b8cb6c4e21d3cfe6207
JosipBuselic <josip.busilica@gmail.com>
2025-04-01 14:33:35 +0200

    slike kategorije

--- js/data.js
+++ js/data.js
@@ -104,7 +104,7 @@ export const data = {
         }]
     },
     {
-        "name":"Trenirke",
+        "name":"Sportske Hlače",
         "products":[{
 
             "name": "Crna Trenirka",
@@ -127,6 +127,182 @@ export const data = {
         {
             "name": "Siva Trenirka",
             "image":"../images/products/trenirke/siva_trenirka.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Bijela Trenirka",
+            "image":"../images/products/trenirke/bijela_trenirka.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        }]
+    },
+    {
+        "name":"Jakne",
+        "products":[{
+
+            "name": "Crna jakna",
+            "image":"../images/products/jakne/crna_jakna.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Bijela jakna",
+            "image":"../images/products/jakne/bijela_jakna.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Plava jakna",
+            "image":"../images/products/jakne/plava_jakna.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Smeđa jakna",
+            "image":"../images/products/jakne/smeda_jakna.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Zelena jakna",
+            "image":"../images/products/jakne/zelena_jakna.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        }]
+    },
+    {
+        "name":"Dukserice",
+        "products":[{
+
+            "name": "Bijela dukserica",
+            "image":"../images/products/dukserice/bijela_duksa.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Crna dukserica",
+            "image":"../images/products/dukserice/crna_duksa.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Crvena dukserica",
+            "image":"../images/products/dukserice/crvena_duksa.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Siva dukserica",
+            "image":"../images/products/dukserice/siva_duksa.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Žuta dukserica",
+            "image":"../images/products/dukserice/zuta_duksa.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        }]
+    },
+    {
+        "name":"Kratke Hlače",
+        "products":[{
+
+            "name": "Bijele kratke hlače",
+            "image":"../images/products/kratkeHlace/bijele_kratkeHlace.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Bež kratke hlače",
+            "image":"../images/products/kratkeHlace/bez_kratkeHlace.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Plave kratke hlače",
+            "image":"../images/products/kratkeHlace/plave_kratkeHlace.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Zelene kratke hlače",
+            "image":"../images/products/kratkeHlace/zelene_kratkeHlace.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "žute kratke hlače",
+            "image":"../images/products/kratkeHlace/zute_kratkeHlace.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        }]
+    },
+    {
+        "name":"Sportske Majice",
+        "products":[{
+
+            "name": "Crvena kratke hlače",
+            "image":"../images/products/sportskeMajice/crvena_sportskaMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Crna kratke hlače",
+            "image":"../images/products/sportskeMajice/crna_sportskaMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Narancasta kratke hlače",
+            "image":"../images/products/sportskeMajice/narancasta_sportskaMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Zelena kratke hlače",
+            "image":"../images/products/sportskeMajice/zelena_sportskaMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Plava kratke hlače",
+            "image":"../images/products/sportskeMajice/plava_sportskaMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        }]
+    },
+    {
+        "name":"Polo Majice",
+        "products":[{
+
+            "name": "Crvena polo majica",
+            "image":"../images/products/poloMajice/crvena_poloMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Bijela polo majica",
+            "image":"../images/products/poloMajice/bijela_poloMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Plava polo majica",
+            "image":"../images/products/poloMajice/plava_poloMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Roza polo majica",
+            "image":"../images/products/poloMajice/roza_poloMajica.png",
+            "size":["XS","S","M","L","XL","XXL"],
+            "price": 29.99,
+        },
+        {
+            "name": "Žuta polo majica",
+            "image":"../images/products/poloMajice/zuta_poloMajica.png",
             "size":["XS","S","M","L","XL","XXL"],
             "price": 29.99,
         }]
